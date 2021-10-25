6628c6f7a5c7a5b699bd4f144bc95c83c4dca59d
h33h <beolki102@gmail.com>
2021-10-25 16:19:55 +0300

    Initial Commit

---
+++ VideoPlayer/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  VideoPlayer
+//
+//  Created by XXX on 25.10.21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
