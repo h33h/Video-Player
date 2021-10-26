69ea39d1bf1b803475e45c0add4af366aa5e2cbe
h33h <beolki102@gmail.com>
2021-10-26 23:59:04 +0300

    AVFoundation setup & clean

--- VideoPlayer/AppDelegate.swift
+++ VideoPlayer/AppDelegate.swift
@@ -6,23 +6,23 @@
 //
 
 import UIKit
+import AVFoundation
 
 @main
 class AppDelegate: UIResponder, UIApplicationDelegate {
-
     func application(_ application: UIApplication,
                      didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
-        return true
+        let audioSession = AVAudioSession.sharedInstance()
+            do {
+                try audioSession.setCategory(.playback, mode: .moviePlayback)
+            } catch {
+                print("Setting category to AVAudioSessionCategoryPlayback failed.")
+            }
+            return true
     }
-
-    // MARK: UISceneSession Lifecycle
-
     func application(_ application: UIApplication,
                      configurationForConnecting connectingSceneSession: UISceneSession,
                      options: UIScene.ConnectionOptions) -> UISceneConfiguration {
         return UISceneConfiguration(name: "Default Configuration", sessionRole: connectingSceneSession.role)
-    }
-
-    func application(_ application: UIApplication, didDiscardSceneSessions sceneSessions: Set<UISceneSession>) {
     }
 }
